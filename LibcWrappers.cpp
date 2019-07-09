a090f406b8efcc8d5aa1f22063f265e4cc59d6ba
Sebastian Poeplau <sebastian.poeplau@eurecom.fr>
2019-07-09 10:39:34 +0200

    More libc handlers and some fixes

--- LibcWrappers.cpp
+++ LibcWrappers.cpp
@@ -2,8 +2,8 @@
 #include <cstdlib>
 #include <cstring>
 
-#include <unistd.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 #include "Runtime.h"
 
@@ -19,7 +19,7 @@ extern "C" {
 void *SYM(malloc)(size_t size) {
   auto result = malloc(size);
   _sym_set_return_expression(_sym_build_integer(
-      reinterpret_cast<uint64_t>(result), sizeof(result) * 8));
+      reinterpret_cast<uintptr_t>(result), sizeof(result) * 8));
   auto shadow = static_cast<Z3_ast *>(malloc(size * sizeof(Z3_ast)));
   _sym_register_memory(static_cast<uint8_t *>(result), shadow, size);
   return result;
@@ -29,7 +29,7 @@ void *SYM(mmap)(void *addr, size_t len, int prot, int flags, int fildes,
                 off_t off) {
   auto result = mmap(addr, len, prot, flags, fildes, off);
   _sym_set_return_expression(_sym_build_integer(
-      reinterpret_cast<uint64_t>(result), sizeof(result) * 8));
+      reinterpret_cast<uintptr_t>(result), sizeof(result) * 8));
   return result;
 }
 
@@ -41,7 +41,7 @@ char *SYM(getenv)(const char *name) {
   auto result = getenv(name);
   // TODO register string memory?
   _sym_set_return_expression(_sym_build_integer(
-      reinterpret_cast<uint64_t>(result), sizeof(result) * 8));
+      reinterpret_cast<uintptr_t>(result), sizeof(result) * 8));
   return result;
 }
 
@@ -59,6 +59,24 @@ void *SYM(memcpy)(void *dest, const void *src, size_t n) {
   auto result = memcpy(dest, src, n);
   _sym_memcpy(static_cast<uint8_t *>(dest), static_cast<const uint8_t *>(src),
               n);
+  _sym_set_return_expression(_sym_build_integer(
+      reinterpret_cast<uintptr_t>(result), sizeof(result) * 8));
+  return result;
+}
+
+void *SYM(memset)(void *s, int c, size_t n) {
+  auto result = memset(s, c, n);
+  _sym_memset(static_cast<uint8_t *>(s), _sym_build_integer(c, sizeof(c) * 8),
+              n);
+  _sym_set_return_expression(_sym_build_integer(
+      reinterpret_cast<uintptr_t>(result), sizeof(result) * 8));
+  return result;
+}
+
+int SYM(select)(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+                struct timeval *timeout) {
+  auto result = select(nfds, readfds, writefds, exceptfds, timeout);
+  _sym_set_return_expression(_sym_build_integer(result, sizeof(result) * 8));
   return result;
 }
 }
