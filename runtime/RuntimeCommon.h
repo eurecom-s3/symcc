9fde348d5867f4ed1d44874ee374526e37601406
Sebastian Poeplau <sebastian.poeplau@eurecom.fr>
2020-06-16 15:21:23 +0200

    Add license headers and a license file

--- runtime/RuntimeCommon.h
+++ runtime/RuntimeCommon.h
@@ -8,6 +8,20 @@
 // Whoever uses this file has to define the type "SymExpr" first; we use it to
 // keep this header independent of the back-end implementation.
 
+// This file is part of SymCC.
+//
+// SymCC is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// SymCC is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// SymCC. If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef RUNTIMECOMMON_H
 #define RUNTIMECOMMON_H
 
--- test/file_input.c
+++ test/file_input.c
@@ -1,3 +1,17 @@
+// This file is part of SymCC.
+//
+// SymCC is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// SymCC is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// SymCC. If not, see <https://www.gnu.org/licenses/>.
+
 // RUN: /bin/echo -ne "\x05\x00\x00\x00aaaa" > %T/%basename_t.input
 // RUN: %symcc -O2 %s -o %t
 // RUN: env SYMCC_INPUT_FILE=%T/%basename_t.input %t %T/%basename_t.input 2>&1 | %filecheck %s
