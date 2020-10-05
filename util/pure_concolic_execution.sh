7c77485af8235222d44ea2941655b853779bdcf5
Sebastian Poeplau <sebastian.poeplau@mailbox.org>
2020-10-05 19:40:23 +0200

    Fix several bugs in the script for pure concolic execution

--- util/pure_concolic_execution.sh
+++ util/pure_concolic_execution.sh
@@ -41,7 +41,7 @@ fi
 
 # Create temporary directories for the current and next generations of inputs
 work_dir=$(mktemp -d)
-mkdir $work_dir/{cur,next}
+mkdir $work_dir/{next,symcc_out}
 if [[ -v out ]]; then
     mkdir -p $out
 fi
@@ -52,14 +52,14 @@ function cleanup() {
 
 trap cleanup EXIT
 
-# Move files from the source directory into our current generation and possibly
+# Move files from the source directory into the next generation and possibly
 # make a copy in $out
 function import() {
     source_dir=$1
-    if [ "$(ls -A $source_dir)" ]; then
+    if [ -n "$(ls -A $source_dir)" ]; then
         for new_case in $source_dir/*; do
-            dest=$work_dir/cur/$(sha256sum $new_case | cut -d' ' -f1)
-            mv $new_case $dest
+            dest=$work_dir/next/$(sha256sum $new_case | cut -d' ' -f1)
+            cp $new_case $dest
 
             if [[ -v out ]]; then
                 cp $dest $out
@@ -69,7 +69,7 @@ function import() {
 }
 
 # Set up the environment
-export SYMCC_OUTPUT_DIR=$work_dir/next
+export SYMCC_OUTPUT_DIR=$work_dir/symcc_out
 export SYMCC_ENABLE_LINEARIZATION=1
 export SYMCC_AFL_COVERAGE_MAP=$work_dir/map
 
@@ -77,22 +77,26 @@ export SYMCC_AFL_COVERAGE_MAP=$work_dir/map
 gen_count=0
 while true; do
     # Initialize the generation
-    import $work_dir/next      # created in the previous generation
-    import $in                 # new files from the input directory (if any)
+    import $in                  # new files from the input directory (if any)
+    mv $work_dir/{next,cur}
+    mkdir $work_dir/next
 
     # Run it (or wait if there's nothing to run on)
-    if [ "$(ls -A $work_dir/cur)" ]; then
+    if [ -n "$(ls -A $work_dir/cur)" ]; then
         echo "Generation $gen_count..."
         for f in $work_dir/cur/*; do
             if [[ "$target " =~ " @@ " ]]; then
-                SYMCC_INPUT_FILE=$f $timeout ${target[@]/@@/$f} >/dev/null 2>&1
+                env SYMCC_INPUT_FILE=$f $timeout ${target[@]/@@/$f} >/dev/null 2>&1
             else
                 cat $f | $timeout $target >/dev/null 2>&1
             fi
 
+            # Make the new test cases part of the next generation
+            import $work_dir/symcc_out
             rm $f
         done
 
+        rmdir $work_dir/cur
         gen_count=$((gen_count+1))
     else
         echo "Waiting for more input..."
