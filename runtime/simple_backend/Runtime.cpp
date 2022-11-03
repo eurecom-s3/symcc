6df45adef5f9d9134687d219ab3959290fff2b66
Emilio Coppa <ercoppa@gmail.com>
2022-11-03 18:37:15 +0100

    fix for issue #108

--- runtime/simple_backend/Runtime.cpp
+++ runtime/simple_backend/Runtime.cpp
@@ -407,10 +407,10 @@ Z3_ast _sym_build_float_to_unsigned_integer(Z3_ast expr, uint8_t bits) {
       g_context, Z3_mk_fpa_round_toward_zero(g_context), expr, bits));
 }
 
-Z3_ast _sym_build_bool_to_bits(Z3_ast expr, uint8_t bits) {
+Z3_ast _sym_build_bool_to_bit(Z3_ast expr) {
   return registerExpression(Z3_mk_ite(g_context, expr,
-                                      _sym_build_integer(1, bits),
-                                      _sym_build_integer(0, bits)));
+                                      _sym_build_integer(1, 1),
+                                      _sym_build_integer(0, 1)));
 }
 
 void _sym_push_path_constraint(Z3_ast constraint, int taken,
