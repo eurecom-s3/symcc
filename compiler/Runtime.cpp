bc06b1505512c0da7c70ac21a883597ff25ca73e
aurelf <aurelien@francillon.net>
2023-11-24 19:47:22 +0100

    clang-format Runtime.cpp

--- compiler/Runtime.cpp
+++ compiler/Runtime.cpp
@@ -192,12 +192,13 @@ Runtime::Runtime(Module &M) {
 /// Decide whether a function is called symbolically.
 bool isInterceptedFunction(const Function &f) {
   static const StringSet<> kInterceptedFunctions = {
-      "malloc", "calloc",  "mmap",     "mmap64",  "open",    "read",
-      "lseek",  "lseek64", "fopen",    "fopen64", "fread",   "fseek",
-      "fseeko", "rewind",  "fseeko64", "getc",    "ungetc",  "memcpy",
-      "memset", "strncpy", "strchr",   "memcmp",  "memmove", "ntohl",
-      "fgets",  "fgetc",   "getchar",  "bcopy",   "bcmp",    "bzero",
-      "freopen", "fread_unlocked", "getc_unlocked", "getline", "getutxent",  
+      "malloc",  "calloc",         "mmap",          "mmap64",  "open",
+      "read",    "lseek",          "lseek64",       "fopen",   "fopen64",
+      "fread",   "fseek",          "fseeko",        "rewind",  "fseeko64",
+      "getc",    "ungetc",         "memcpy",        "memset",  "strncpy",
+      "strchr",  "memcmp",         "memmove",       "ntohl",   "fgets",
+      "fgetc",   "getchar",        "bcopy",         "bcmp",    "bzero",
+      "freopen", "fread_unlocked", "getc_unlocked", "getline", "getutxent",
       "getutent"};
   
   return (kInterceptedFunctions.count(f.getName()) > 0);
